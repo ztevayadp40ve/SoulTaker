5b13f94d1ac6350afab2e5e75cc5cbca6447c77f
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-02-14 17:08:18 +0100

    Delete src directory

--- src/config.py
+++
@@ -1,9 +0,0 @@
-__CONFIG__ = {
-    'webhook' : None,
-    'antidebug' : False,
-    'browsers' : False,
-    'discordtoken' : False,
-    'injection' : False,
-    'startup' : False,
-    'systeminfo' : False,
-}
